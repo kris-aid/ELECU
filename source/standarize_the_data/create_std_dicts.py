1010277c5858eb0ca26788f2b90c74bd4a174bf4
Kristian Mendoza <saidmendoza730@gmail.com>
2024-03-12 22:19:13 -0500

    Creado el metodo para estandarizar provincias

--- source/standarize_the_data/create_std_dicts.py
+++ source/standarize_the_data/create_std_dicts.py
@@ -76,8 +76,6 @@ class Standard_Dictionaries:
                         df["ANIO"] = año[0]
                         # Agregar el DataFrame al DataFrame vacío
                         df_dignidades = pd.concat([df_dignidades, df])
-                        print("gola")
-                        print(df_dignidades)
         return df_dignidades
     
     def change_to_std_dignidades(self):
@@ -119,21 +117,32 @@ class Standard_Dictionaries:
         # Si el nombre de la dignidad es CONCEJALES MUNICIPALES vamos a cambiarlo a CONCEJO MUNICIPAL
         
         dict_equivalencias= {
-                            "PRESIDENCIA": ["PRESIDENTA/E Y VICEPRESIDENTA/E", "PRESIDENTE Y VICEPRESIDENTE"],
-                            "PREFECTURA": ["PREFECTA / PREFECTO","PREFECTO PROVINCIAL", "PREFECTO Y VICEPREFECTO"],
-                            "ALCALDIA": ["ALCALDES", "ALCALDE MUNICIPAL", "ALCALDES MUNICIPALES", "ALCALDESA / ALCALDE"],
-                            "ASAMBLEA NACIONAL":["ASAMBLEISTAS NACIONALES"],
-                            "ASAMBLEA PROVINCIAL": ["ASAMBLEISTAS PROVINCIALES", "ASAMBLEISTAS PROVINCIALES Y DEL EXTERIOR","DIPUTADOS PROVINCIALES"],
-                            "ASAMBLEA CIRCUNSCRIPCION":["ASAMBLEISTAS POR CIRCUNSCRIPCION"],
-                            "CONCEJO URBANO": ["CONCEJALES MUNICIPALES", "CONCEJALES URBANOS", "CONCEJAL MUNICIPAL"],
-                            "CONCEJO RURAL": ["CONCEJALES RURALES"],
-                            "CONCEJO PROVINCIAL": ["CONSEJEROS PROVINCIALES","CONSEJERO PROVINCIAL"],
-                            "JUNTA PARROQUIAL": ["JUNTAS PARROQUIALES","MIEMBROS JUNTAS PARROQUIALES","VOCALES DE JUNTA PARROQUIAL",
-                                                "VOCALES DE JUNTAS PARROQUIALES","VOCALES JUNTAS PARROQUIALES"],
-                            "PARLAMENTO ANDINO": ["PARLAMENTARIOS ANDINOS"],
-                            "CPCCS M": ["CPCCS (MUJERES)"],
-                            "CPCCS H": ["CPCCS (HOMBRES)"],
-                            "CPCCS NAC/EXT": ["CPCCS (NAC/EXT)"]}
+                    "PRESIDENCIA": ["PRESIDENTA/E Y VICEPRESIDENTA/E", "PRESIDENTE Y VICEPRESIDENTE"],
+                    "PREFECTURA": ["PREFECTA / PREFECTO","PREFECTO PROVINCIAL", "PREFECTO Y VICEPREFECTO"],
+                    "ALCALDIA": ["ALCALDES", "ALCALDE MUNICIPAL", "ALCALDES MUNICIPALES", "ALCALDESA / ALCALDE"],
+                    "ASAMBLEA NACIONAL":["ASAMBLEISTAS NACIONALES"],
+                    "ASAMBLEA PROVINCIAL": ["ASAMBLEISTAS PROVINCIALES", "ASAMBLEISTAS PROVINCIALES Y DEL EXTERIOR","DIPUTADOS PROVINCIALES"],
+                    "ASAMBLEA CIRCUNSCRIPCION":["ASAMBLEISTAS POR CIRCUNSCRIPCION"],
+                    "CONCEJO URBANO": ["CONCEJALES MUNICIPALES", "CONCEJALES URBANOS", "CONCEJAL MUNICIPAL"],
+                    "CONCEJO RURAL": ["CONCEJALES RURALES"],
+                    "CONCEJO PROVINCIAL": ["CONSEJEROS PROVINCIALES","CONSEJERO PROVINCIAL"],
+                    "JUNTA PARROQUIAL": ["JUNTAS PARROQUIALES","MIEMBROS JUNTAS PARROQUIALES","VOCALES DE JUNTA PARROQUIAL",
+                                        "VOCALES DE JUNTAS PARROQUIALES","VOCALES JUNTAS PARROQUIALES"],
+                    "PARLAMENTO ANDINO": ["PARLAMENTARIOS ANDINOS"],
+                    "CPCCS M": ["CPCCS (MUJERES)"],
+                    "CPCCS H": ["CPCCS (HOMBRES)"],
+                    "CPCCS NAC/EXT": ["CPCCS (NAC/EXT)"]}
+                    
+        # # Find the maximum length among all lists
+        # max_length = max(len(lst) for lst in dict_equivalencias.values())
+
+        # # Fill shorter lists with NaN to make them equal length
+        # for key, value in dict_equivalencias.items():
+        #     dict_equivalencias[key] = value + [float('nan')] * (max_length - len(value))
+
+        # # Create a DataFrame
+        # df = pd.DataFrame.from_dict(dict_equivalencias, orient='index')
+        # df.T.to_csv("data_csv/Codigos_estandar/dignidades/equivalencias_dignidades_T.csv", index=True)
         
         #Vamos a cambiar los nombres de las dignidades
         #Primero vamos a cambiar los nombres de las dignidades que no son los mismos que los del diccionario
@@ -205,15 +214,70 @@ class Standard_Dictionaries:
                         df_provincias = pd.concat([df_provincias, df])
         return df_provincias
         
-       
+    def change_to_std_provincias(self):
+        '''
+        Cambia los nombres y códigos de las provincias a los nombres y códigos estandarizados
+        
+        Paramaters
+        ----------
+            - df_provincias: DataFrame
+                DataFrame con los códigos de provincias de las elecciones
+                
+        
+        Returns
+        -------
+            - df_provincias: DataFrame
+                DataFrame con los códigos de provincias de las elecciones estandarizados    
+    
+        '''
+        #Diccionario de nombres de provincias del exterior y sus equivalentes
+        dict_equivalencias_exterior = {
+            "LATAM, CARIBE, AFRICA": ["AMERICA LATINA, EL CARIBE Y AFRICA", "AMERICA LATINA", "AMERICA LATINA EL CARIBE Y AFRICA","LATINOAMERICA, EL CARIBE Y AFRICA"],
+            "NORTE-AMERICA": ["CANADA Y ESTADOS UNIDOS", "EE.UU CANADA", "EEUU Y CANADA", "EE.UU Y CANADA"],
+            "EUROPA, ASIA, OCEANIA": ["EUROPA" , "ASIA, OCEANIA", "EUROPA ASIA OCEANIA", "EUROPA, ASIA Y OCEANIA","EUROPA ASIA Y OCEANIA","EUROPA, OCEANIA Y ASIA"],
+            "VOTO EXTERIOR": ["VOTO EXTERIOR"],
+            "ECUADOR": ["ECUADOR","NACION"]}
+        #fill the dictionary with NaN to make them equal length
+        # Find the maximum length among all lists
+        # max_length = max(len(lst) for lst in dict_equivalencias_exterior.values())
+        # # Fill shorter lists with NaN to make them equal length
+        # for key, value in dict_equivalencias_exterior.items():
+        #     dict_equivalencias_exterior[key] = value + [float('nan')] * (max_length - len(value))
+        # # Create a DataFrame   
+        #pd.DataFrame(dict_equivalencias_exterior).to_csv("data_csv/Codigos_estandar/provincias/equivalencias_exterior.csv", index=False)
+        
+        dict_provincias= { "PROVINCIA_CODIGO":["P00","P01","P02","P03","P04","P05","P06","P07","P08","P09","P10",
+                                               "P11","P12","P13","P14","P15","P16","P17","P18","P19","P20","P21","P22","P23","P24","P25","P26","P27","P28"],
+                           "PROVINCIA_NOMBRE":["ECUADOR","AZUAY", "BOLIVAR", "CANAR", "CARCHI", "COTOPAXI", "CHIMBORAZO", "EL ORO", "ESMERALDAS", "GUAYAS", "IMBABURA",
+                                               "LOJA", "LOS RIOS", "MANABI", "MORONA SANTIAGO", "NAPO", "PASTAZA", "PICHINCHA", "TUNGURAHUA", "ZAMORA CHINCHIPE", "GALAPAGOS", "SUCUMBIOS", "ORELLANA", "STO DGO TSACHILAS", "SANTA ELENA", "VOTO EXTERIOR","EUROPA, ASIA, OCEANIA","NORTE-AMERICA","LATAM, CARIBE, AFRICA"],
+                           "PROVINCIA_CODIGO_OLD":[0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                                                   11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28]}      
+        
+        #pd.DataFrame(dict_provincias).to_csv("data_csv/Codigos_estandar/provincias/provincias_std.csv", index=False)
+        
+        #Vamos a cambiar los nombres de las dignidades
+        #Primero vamos a cambiar los nombres de las dignidades que no son los mismos que los del diccionario
+        for key, value in dict_equivalencias_exterior.items():
+            for v in value:
+                self.df_provincias.loc[self.df_provincias["PROVINCIA_NOMBRE"]==v, "PROVINCIA_NOMBRE"] = key
+        
+        # Vamos a hacer un left join con el DataFrame de las provincias estandarizadas
+        self.df_provincias = self.df_provincias.merge(pd.DataFrame(dict_provincias), on="PROVINCIA_NOMBRE", how="left")
+        # Y nos quedamos con los códigos estandarizados
+        self.df_provincias["PROVINCIA_CODIGO"] = self.df_provincias["PROVINCIA_CODIGO_y"]
+        self.df_provincias["PROVINCIA_CODIGO_OLD"] = self.df_provincias["PROVINCIA_CODIGO_x"]
+        # Eliminamos las columnas que no necesitamos
+        self.df_provincias = self.df_provincias.drop(columns=["PROVINCIA_CODIGO_x", "PROVINCIA_CODIGO_y", "ANIO"])
+        print(self.df_provincias)
         
     
-    
+        
 
 #Test del metodo para el año 2013
-input_folder = "data_csv/generales/2002/diccionarios"
+input_folder = "data_csv/seccionales/2023/diccionarios"
 std_dicts = Standard_Dictionaries(input_folder)
 #print(std_dicts.df_dignidades)
-std_dicts.change_to_std_dignidades()
-print(std_dicts.df_dignidades)
-#print(std_dicts.df_provincias)
+#std_dicts.change_to_std_dignidades()
+#print(std_dicts.df_dignidades)
+print(std_dicts.df_provincias)
+std_dicts.change_to_std_provincias()
