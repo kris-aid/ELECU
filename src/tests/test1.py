7e83adf1a844bd7fed2fba58e26d9cd6027e2131
Kristian Mendoza <saidmendoza730@gmail.com>
2024-04-01 20:37:29 -0500

    Agregado de pruebas para la creación de diccionarios estándar

---
+++ src/tests/test1.py
@@ -0,0 +1,7 @@
+from src.open_elec.std_data.create_std_dicts import Standard_Dictionaries
+
+def test_create_std_dicts():
+    input_folder = "data_csv/seccionales/2023/diccionarios"
+    std_dicts = Standard_Dictionaries(input_folder)
+    print(std_dicts.df_provincias)
+    std_dicts.change_to_std_provincias()
