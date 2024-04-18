ab589d016f92e0ea9c7a53ad87d5bce9b567d418
Kristian Mendoza <saidmendoza730@gmail.com>
2024-04-18 00:33:58 -0500

    Agregados archivos CSV para nombres y códigos estándar de sexo y electores en el registro electoral.

--- src/open_elec/standarize_data/restructure_results.py
+++ src/open_elec/standarize_data/restructure_results.py
@@ -107,10 +107,8 @@ class Standarized_Results:
             self.df_registro.rename(columns={'GRANDES_GRUPOS_DE_EDAD_1V': 'G_EDAD'}, inplace=True)
         elif 'G_EDAD' in self.df_registro.columns:
             pass
-        # Crear una columna con el año
-        for col in self.df_registro.columns:
-            if 'G_EDAD' in col:
-                self.df_registro[col] = self.df_registro[col].astype(str)
+       
+        self.df_registro['G_EDAD'] = self.df_registro['G_EDAD'].astype(str)
         
         if 'SEXO' in self.df_registro.columns:
             pass
@@ -160,8 +158,102 @@ class Standarized_Results:
     
     def recuperar_resultados(self):
         '''
-        Recupera los resultados de los archivos de resultados.'''
-        for 
+        Recupera los resultados de los archivos de resultados.
+        '''
+        df_resultados = pd.DataFrame()
+        for root, dirs, files in os.walk(self.input_folder):
+            for file in files:
+                if file.endswith(".csv"):
+                    if file.startswith("resultados"):
+                        # Construir el path del archivo
+                        file_path = os.path.join(root, file)
+                        # Leer el archivo en un DataFrame
+                        df = pd.read_csv(file_path)
+                        df.columns = [unidecode.unidecode(col) for col in df.columns]
+                        anio = file.split("resultados")[1].split(".")[0][1:]
+                        df["ANIO"] = anio
+                        df_resultados = pd.concat([df_resultados, df])
+        return df_resultados
+    
+    def change_resultados(self):
+        '''
+        Cambia los resultados de los archivos de resultados.
+        '''
+        # Cambiar el nombre de las columnas
+        self.df_resultados.columns = [unidecode.unidecode(col) for col in self.df_resultados.columns]
+
+        #Colocar el nombre correcto de las columna SEXO
+        if 'SEX_JUNTA'  in self.df_resultados.columns:
+            self.df_resultados.rename(columns={'SEX_JUNTA': 'SEXO'}, inplace=True)
+        elif 'JUNTA_SEXO' in self.df_resultados.columns:
+            self.df_resultados.rename(columns={'JUNTA_SEXO': 'SEXO'}, inplace=True)  
+        elif 'SEXO' in self.df_resultados.columns:
+            pass    
+        self.df_resultados['SEXO'] = self.df_resultados['SEXO'].astype(str)
+        
+        
+        # Colocar el nombre correcto de las columnas de la circunscripcion
+        if 'CIRCUSCRIPCION_CODIGO_ORIGINAL' in self.df_resultados.columns:
+            self.df_resultados.rename(columns={'CIRCUSCRIPCION_CODIGO_ORIGINAL': 'CIRCUNSCRIPCION_CODIGO'}, inplace=True)
+        else:
+            pass
+        
+        # Colocar el nombre correcto de las columnas de blancos y nulos
+        if "VOTOS_EN_BLANCO" in self.df_resultados.columns:
+            self.df_resultados.rename(columns={'VOTOS_EN_BLANCO': 'BLANCOS'}, inplace=True)
+        else:
+            pass
+        
+        if "VOTOS_NULOS" in self.df_resultados.columns:
+            self.df_resultados.rename(columns={'VOTOS_NULOS': 'NULOS'}, inplace=True)
+        else:
+            pass
+        
+        # colocar el nombre del codigo de candidato
+        if 'CODIGO_CANDIDATO_RESULTADO' in self.df_resultados.columns:
+            self.df_resultados.rename(columns={'CODIGO_CANDIDATO_RESULTADO': 'CODIGO_CANDIDATO'}, inplace=True)
+        else:
+            pass
+        # Colocar el nombre de la columna de votos
+        if 'VOTOS' in self.df_resultados.columns:
+            pass
+        elif 'CANDIDATO_VOTOS' in self.df_resultados.columns:
+            self.df_resultados.rename(columns={'CANDIDATO_VOTOS': 'VOTOS'}, inplace=True)
+        
+        rows_to_preserve = ["PROVINCIA_CODIGO", "CANTON_CODIGO", "PARROQUIA_CODIGO", "SEXO"]
+        
+        #drop columns if they are present
+        if 'NUMERO_DE_ACTAS' in self.df_resultados.columns:
+            self.df_resultados.drop("NUMERO_DE_ACTAS", axis=1, inplace=True)
+        if 'NUMERO_DE_JUNTAS' in self.df_resultados.columns:
+            self.df_resultados.drop("NUMERO_DE_JUNTAS", axis=1, inplace=True)
+        if 'CANDIDATO_ESTADO' in self.df_resultados.columns:
+            self.df_resultados.drop("CANDIDATO_ESTADO", axis=1, inplace=True)
+        
+        # preserve the columns if they are present
+        if 'OP_VOTO_EN_PLANCHA' in self.df_resultados.columns:
+            rows_to_preserve.append('OP_VOTO_EN_PLANCHA')
+        
+        if 'CIRCUNSCRIPCION_CODIGO' in self.df_resultados.columns:
+            rows_to_preserve.append('CIRCUNSCRIPCION_CODIGO')
+        if 'CIRCUNSCRIPCION_NOMBRE' in self.df_resultados.columns:
+            rows_to_preserve.append('CIRCUNSCRIPCION_NOMBRE')
+        if 'SUFRAGANTES' in self.df_resultados.columns:
+            rows_to_preserve.append('SUFRAGANTES')
+            
+        # Now for the SEXO column we want to change the values based on the sexo_names_std in the standarized folder
+        df_sexo_names = self.load_std_data("resultados/sexo_names_std.csv")
+        # retrieve the values of each row in the df_sexo_names as a list
+        for _, row_eq in df_sexo_names.iterrows():
+            row_eq=row_eq.values.tolist()
+            
+            for index, row in self.df_resultados.iterrows():
+                sexo=row['SEXO']
+                if sexo in row_eq:
+                    #print(sexo)
+                    self.df_resultados.at[index,'SEXO']=row_eq[0]
+        
+        
         
 if __name__ == "__main__":
     #get the current directory
@@ -170,8 +262,7 @@ if __name__ == "__main__":
     os.chdir(current_directory)
     print(current_directory)
     #D:\Open-ELEC\data_csv\seccionales\2014\diccionarios
-    input_folder = "../../../data_csv/seccionales/2004"
+    input_folder = "../../../data_csv/generales/2021"
     standarized_folder = "../data/Codigos_estandar/"
     standarized_results = Standarized_Results(input_folder, standarized_folder)
-    print(standarized_results.df_registro)
-    print(standarized_results.change_registro())
+    print(standarized_results.df_resultados)
