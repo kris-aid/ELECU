0fdc4214127399862e3df2761b3669604e1b8f2a
Kristian Mendoza <saidmendoza730@gmail.com>
2025-09-28 17:52:07 -0500

    missing files of the repo

---
+++ elecu/elecu/constants.py
@@ -0,0 +1,6 @@
+#
+# Este documento contiene los paths.
+# Debe se importado con `from paths import *`
+# en todos los documentos en los que se necesite.
+CODIGOS_ESTANDAR_PATH="data/Codigos_estandar"
+
